ea6e583fcfe184ce0b2497b51ad9ba2ebbcc0fe3
Alaeddine <87445066+alaeddineazri@users.noreply.github.com>
2021-10-15 18:23:02 +0100

    Add files via upload

---
+++ DDL-Checkpoint.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Product (
+    Product_id VARCHAR2(20) CONSTRAINT pk_product PRIMARY KEY , 
+    Product_Name VARCHAR2(20) CONSTRAINT nt_product NOT NULL,
+    Price NUMBER CONSTRAINT ck_product CHECK (price>0) );
+
+
+CREATE TABLE Customer (
+    Customer_id VARCHAR2(20) CONSTRAINT pk_customer PRIMARY KEY, 
+    Customer_Name VARCHAR2(20) CONSTRAINT nt_customer NOT NULL , 
+    Customer_Tel NUMBER 
+    );
+
+
+CREATE TABLE Orders ( 
+    CONSTRAINT fk_customer FOREIGN KEY (Costumer_id) REFERENCES Customer (Costumer_id) ,
+    CONSTRAINT fk_product FOREIGN KEY (Product_id) REFERENCES Product (Product_id) , 
+    Quantity Number ,Total_amount NUMBER 
+    );
+
+--! Add a column Category (VARCHAR2(20)) to the PRODUCT table. 
+
+ALTER TABLE Product ADD Category (VARCHAR2(20);
+
+--! Add a column OrderDate (DATE)  to the ORDERS table which have SYSDATE as a default value.
+ALTER TABLE Orders ADD OrderDate (DATE) DEFAULT SYSDATE;
